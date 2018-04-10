b80223a6046f6d163f98a8b2ccdd29460a63a303
elhe <elhed@yandex.ru>
2018-04-10 19:57:14 +0200

    added threading; added list of keywords for each answer

--- src/features_separately.py
+++ src/features_separately.py
@@ -1,14 +1,10 @@
 import matplotlib.pyplot as plt
 
-
 accuracies = [0.677217928004, 0.691322937824, 0.684630030776, 0.693957037441, 0.688757256379, 0.700518340123, 0.700338038117, 0.679394833249 ]
-x_values = ["allR+B", "allR", "BLUE", "R1", "R2", 'RSU', 'R12', "RSU+BLEU"] #, "R12+BLEU"]
-plt.xlabel('Feature combination')
-plt.ylabel('Average accuracy')
-plt.grid(True)
-plt.plot(x_values, accuracies)
-plt.plot(x_values, accuracies, 'bo')
-plt.show()
+features = ["allR+B", "allR", "BLUE", "R1", "R2", 'RSU', 'R12', "RSU+BLEU"] #, "R12+BLEU"]
+y_axis = range(1, 9)
 
-a = [1,2,3]
-print(a[:2])
+plt.plot(y_axis, accuracies)
+plt.plot(y_axis, accuracies, "bo")
+plt.xticks(y_axis, features)
+plt.show()
--- src/main.py
+++ src/main.py
@@ -5,11 +5,14 @@ from nltk.stem import WordNetLemmatizer
 from string import digits, punctuation
 from pythonrouge.pythonrouge import Pythonrouge
 import codecs
+import multiprocessing
 from sklearn.ensemble import RandomForestClassifier
+from itertools import repeat
 import numpy as np
 import matplotlib.pyplot as plt
 
 def filter(sentence, spelling):
+    #TODO: add such dictionary {1: "one", 2: "two"...} 0 = "null" or "zero"
     wordnet_lemmatizer = WordNetLemmatizer()
     sentence = nltk.word_tokenize(sentence.lower())
     all_stops = set(stopwords.words('english')) | set(punctuation) | set(digits)
@@ -20,75 +23,72 @@ def filter(sentence, spelling):
     filtered_words = filtered_words[:][1:]
     return filtered_words
 
-def get_answers(path):
+def get_from_file(path):
     with codecs.open(path, "r") as fIn:
-        answers = [ answer.rstrip() for answer in fIn.readlines()]
-        return answers
+        strings = [ strings.rstrip() for strings in fIn.readlines()]
+        return strings
 
-def get_grades(path):
-    with codecs.open(path, "r") as fIn:
-        grades = [ grade.rstrip() for grade in fIn.readlines()]
-        return grades
+def learning_scores(question_index, answer_array, correct_answers):
+    clf = RandomForestClassifier(n_estimators=5)
+    print(question_index)
+    for i in range(len(answer_array[question_index][1])):
+        answer_array[0][0][i] = filter(answer_array[question_index][0][i], True)
+    reference = [[[' '.join(word for word in correct_answers[question_index])]]]
+    features = []
+    for i in range(len(answer_array[question_index][1])):
+        # BLUE calculation:
+        BLEU_score = nltk.translate.bleu_score.sentence_bleu([correct_answers[question_index]],
+                                                             answer_array[question_index][0][i])
+        # ROUGE calculation:
+        # print(answer_array[question_index][0][i])
+        summary = [[' '.join(word for word in answer_array[question_index][0][i])]]
+        rouge = Pythonrouge(summary_file_exist=False,
+                            summary=summary, reference=reference,
+                            n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
+                            recall_only=True, stemming=False, stopwords=False,
+                            word_level=True, length_limit=True, length=50,
+                            use_cf=False, cf=95, scoring_formula='average',
+                            resampling=True, samples=1000, favor=True, p=0.5)
+        ROUGE_score = rouge.calc_score()
+        # {'ROUGE-1': 0.16667, 'ROUGE-2': 0.0, 'ROUGE-SU4': 0.05}
+        features.append(list(ROUGE_score.values())[3])
+        # features[i].append(BLEU_score)
+        # features.append(BLEU_score)
+        # print(answer_array[question_index][1][i])
+        # print(features[i])
+    # diff_scores = []
+    # for split_position in range(3, 24, 2):
+    split_position = 13  # for it gives the best accuracy
+    features = np.asarray(features)
+    features = features.reshape(-1, 1)
+    clf.fit(features[0:split_position], answer_array[question_index][1][0:split_position])
+    print("RF")
+    score = clf.score(features[split_position:len(answer_array[question_index][1])],
+                            answer_array[question_index][1][split_position:len(answer_array[question_index][1])])
+    print(score)
+    return score
+    #     diff_scores.append(clf.score(features[split_position:len(answer_array[question_index][1])],
+    #                         answer_array[question_index][1][split_position:len(answer_array[question_index][1])]))
+    # scores_diff_split.append(diff_scores)
+
 
 def main():
+    pool = multiprocessing.Pool(processes = multiprocessing.cpu_count())
     question_number = [7,7,7,7,4,7,7,7,7,7,10,10]
-    correct_answers = get_answers('../../data/Mohler/raw/answers')
+    data_folder = '../../data/Mohler/'
+    correct_answers = get_from_file(data_folder + 'raw/answers')
     for i in range(len(correct_answers)):
         correct_answers[i] = filter(correct_answers[i], True)
     correct_answer_index = 0
     answer_array = []
     for chapter in range(1, len(question_number) + 1):
         for question in range(1, question_number[chapter - 1] + 1):
-            answers = get_answers('../../data/Mohler/raw/' + str(chapter) + '.' + str(question))
-            grades = get_grades('../../data/Mohler/scores/' + str(chapter) + '.' + str(question) + '/other')
+            answers = get_from_file(data_folder + 'raw/' + str(chapter) + '.' + str(question))
+            grades = get_from_file(data_folder + 'scores/' + str(chapter) + '.' + str(question) + '/other')
             answer_array.append([answers, grades])
             correct_answer_index += 1
-
-    question_index = 0
-    scores_diff_split = []
-    scores = []
-    clf = RandomForestClassifier(n_estimators=25)
-    for question_index in range(87):
-        print(question_index)
-        for i in range(len(answer_array[question_index][1])):
-            answer_array[0][0][i] = filter(answer_array[question_index][0][i], True)
-        reference = [[[' '.join(word for word in correct_answers[question_index])]]]
-        features = []
-        for i in range(len(answer_array[question_index][1])):
-            # BLUE calculation:
-            BLEU_score = nltk.translate.bleu_score.sentence_bleu([correct_answers[question_index]], answer_array[question_index][0][i])
-            # ROUGE calculation:
-            #print(answer_array[question_index][0][i])
-            summary = [[' '.join(word for word in answer_array[question_index][0][i])]]
-            rouge = Pythonrouge(summary_file_exist=False,
-                                summary=summary, reference=reference,
-                                n_gram=2, ROUGE_SU4=True, ROUGE_L=True,
-                                recall_only=True, stemming=False, stopwords=False,
-                                word_level=True, length_limit=True, length=50,
-                                use_cf=False, cf=95, scoring_formula='average',
-                                resampling=True, samples=1000, favor=True, p=0.5)
-            ROUGE_score = rouge.calc_score()
-            # {'ROUGE-1': 0.16667, 'ROUGE-2': 0.0, 'ROUGE-SU4': 0.05}
-            features.append( [list(ROUGE_score.values())[0], list(ROUGE_score.values())[1], BLEU_score])
-            #features[i].append(BLEU_score)
-            #features.append(BLEU_score)
-            #print(answer_array[question_index][1][i])
-            #print(features[i])
-        # diff_scores = []
-        # for split_position in range(3, 24, 2):
-        split_position = 13 # for it gives the best accuracy
-        #features = np.asarray(features)
-        #features = features.reshape(-1, 1)
-        clf.fit(features[0:split_position], answer_array[question_index][1][0:split_position])
-        print("RF")
-        scores.append(clf.score(features[split_position:len(answer_array[question_index][1])],
-                        answer_array[question_index][1][split_position:len(answer_array[question_index][1])]))
-        print(scores[question_index])
-        #     diff_scores.append(clf.score(features[split_position:len(answer_array[question_index][1])],
-        #                         answer_array[question_index][1][split_position:len(answer_array[question_index][1])]))
-        # scores_diff_split.append(diff_scores)
-
-
+    scores = pool.starmap(learning_scores, zip(range(len(correct_answers)), repeat(answer_array), repeat(correct_answers)))
+    print(len(scores))
     print("average")
     average_score = sum(scores)/len(scores)
     print(average_score)
