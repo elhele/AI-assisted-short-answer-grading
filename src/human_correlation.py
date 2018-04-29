89d746ca4f2d39bb08c7ff8706707babb4d19ee3
Evgeniya Ovchinnikova <evgeniya.ovchinnikova@inf.h-brs.de>
2018-04-29 21:56:31 +0200

    #2 added box plot; n-gram co-occurrence; k-skip-n-gram co-occurrence

--- src/human_correlation.py
+++ src/human_correlation.py
@@ -1,5 +1,9 @@
 import codecs
 import difflib
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+import itertools
 
 def get_grades(path):
     with codecs.open(path, "r") as fIn:
@@ -10,6 +14,9 @@ question_number = [7,7,7,7,4,7,7,7,7,7,10,10]
 correct_answer_index = 0
 grades_array = []
 grades_corr = []
+grades_all_me = []
+grades_all_other = []
+class_names = []
 for chapter in range(1, len(question_number) + 1):
     for question in range(1, question_number[chapter - 1] + 1):
         grades_me = get_grades('../data/Mohler/scores/' + str(chapter) + '.' + str(question) + '/me')
@@ -18,6 +25,61 @@ for chapter in range(1, len(question_number) + 1):
         #print(str(chapter) + '.' + str(question))
         #print(sm.ratio())
         grades_array.append([grades_me, grades_other])
+        for i in range(len(grades_me)):
+            grades_all_me.append(grades_me[i])
+            grades_all_other.append(grades_other[i])
         grades_corr.append(sm.ratio())
 print(sum(grades_corr)/len(grades_corr))
 
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('First grader')
+    plt.xlabel('Second grader')
+
+# Compute confusion matrix
+class_names = ['1', '2','3','4','5']
+print(grades_all_me[0:10])
+cnf_matrix = confusion_matrix(grades_all_me, grades_all_other, labels=['1', '2', '3', '4', '5'])
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
 from autocorrect import spell
+import matplotlib.pyplot as plt
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk import ngrams
+from nltk.util import skipgrams
 from string import digits, punctuation
 from pythonrouge.pythonrouge import Pythonrouge
 import codecs
 import multiprocessing
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from itertools import repeat
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,6 +39,50 @@ def get_from_file(path):
         return strings
 
 
+def ngram_cooccurrence(example, answer, ng, question_index):
+    distance_over_ngrams = 0
+    for n in range(1, ng):
+        answer_ngramed = list(ngrams(answer, n))
+        example_ngramed = list(ngrams(example, n))
+        summ_distance = 0
+        for ngram_of_example in example_ngramed:
+            dist = 0
+            for ngram_of_answer in answer_ngramed:
+                dist_tmp = distance.jaccard(ngram_of_example, ngram_of_answer)
+                if dist_tmp > dist:
+                    dist = dist_tmp
+            summ_distance += dist
+            # if (len(example_ngramed) == 0):
+            #    print("zero!")
+            #    print(example_ngramed)
+            #    print(question_index)
+    try:
+        distance_over_ngrams += summ_distance / len(example_ngramed)
+        return distance_over_ngrams
+    except:
+        return 0
+
+
+def kskip_ngram_cuoccurrence(answer, example, ng, kskip, question_index):
+    distance_over_ngrams = 0
+    for n in range(2, ng):
+        answer_ngramed = list(skipgrams(answer, n, kskip))
+        example_ngramed = list(skipgrams(example, n, kskip))
+        summ_distance = 0
+        for ngram_of_example in example_ngramed:
+            dist = 0
+            for ngram_of_answer in answer_ngramed:
+                dist_tmp = distance.jaccard(ngram_of_example, ngram_of_answer)
+                if dist_tmp > dist:
+                    dist = dist_tmp
+            summ_distance += dist
+    try:
+        distance_over_ngrams += summ_distance / len(example_ngramed)
+        return distance_over_ngrams
+    except:
+        return 0
+
+
 def get_keywords_count(answer, keywords, question_index):
     summ_distance = 0
     for keyword in keywords:
@@ -43,15 +92,15 @@ def get_keywords_count(answer, keywords, question_index):
             if dist_tmp > dist:
                 dist = dist_tmp
         summ_distance += dist
-    if len(keywords) == 0:
-        print("key" + str(keywords) + str(question_index))
-        return 0
-    else:
+    try:
         return summ_distance / len(keywords)
+    except:
+        return 0
 
 
 def get_features(question_index, answer_array, correct_answers, keywords):
-    print("question number:" + str(question_index) + str(keywords[question_index]))
+    print("question number:" + str(question_index))
+    keyword = keywords[question_index]
     reference = [[[' '.join(word for word in correct_answers[question_index])]]]
     features = []
     for i in range(len(answer_array[question_index][1])):
@@ -70,9 +119,10 @@ def get_features(question_index, answer_array, correct_answers, keywords):
         # ROUGE_score = rouge.calc_score()
         # {'ROUGE-1': 0.45455, 'ROUGE-2': 0.2, 'ROUGE-L': 0.36364, 'ROUGE-SU4': 0.24}
         # features.append(list(ROUGE_score.values())[2])
-        keyword_coverage = get_keywords_count(keywords[question_index], answer_array[question_index][0][i],
-                                              question_index)
-        features.append(keyword_coverage)
+        ksip_ngram_cooc = kskip_ngram_cuoccurrence(correct_answers[question_index],
+                                                   answer_array[question_index][0][i], 3, 3, question_index)
+        # features.append(list(ROUGE_score.values())[2])
+        features.append(ksip_ngram_cooc)
     # For one feature classification:
     features = np.asarray(features)
     features = features.reshape(-1, 1)
@@ -82,18 +132,24 @@ def get_features(question_index, answer_array, correct_answers, keywords):
 def main():
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     question_number = [7, 7, 7, 7, 4, 7, 7, 7, 7, 7, 10, 10]
-    data_folder = 'data/Mohler/'
-    answer_array_filepath = data_folder + "/processed/answer_array.pickle"
-    r1_features_filepath = data_folder + "/processed/r1.pickle"
-    r2_features_filepath = data_folder + "/processed/r2.pickle"
-    rL_features_filepath = data_folder + "/processed/rL.pickle"
-    rS4_features_filepath = data_folder + "/processed/rS4.pickle"
-    blue_features_filepath = data_folder + "/processed/blue.pickle"
-    rouge_features_filepath = data_folder + "/processed/rouge.pickle"
-    r12L_features_filepath = data_folder + "/processed/r12L.pickle"
-    keywords_features_filepath = data_folder + "/processed/keywords.pickle"
-    ngrams_features_filepath = data_folder + "/processed/ngrams.pickle"
-    kskip_features_filepath = data_folder + "/processed/kskip.pickle"
+    data_folder = '../../data/Mohler/' # for laptop
+    #data_folder = 'data/Mohler/'  # for comp
+    answer_array_filepath_other = data_folder + "/processed/answer_array.pickle"
+    answer_array_filepath_me = data_folder + "/processed/answer_array_me.pickle"
+    answer_array_filepath_ave = data_folder + "/processed/answer_array_ave.pickle"
+    answer_array_filepath_discard = data_folder + "/processed/answer_array_discard.pickle"
+    r1_features_filepath_other = data_folder + "/processed/r1.pickle"
+    r2_features_filepath_other = data_folder + "/processed/r2.pickle"
+    rL_features_filepath_other = data_folder + "/processed/rL.pickle"
+    rL_features_filepath_ave = data_folder + "/processed/rL_ave.pickle"
+    rL_features_filepath_me = data_folder + "/processed/rL_me.pickle"
+    rS4_features_filepath_other = data_folder + "/processed/rS4.pickle"
+    blue_features_filepath_other = data_folder + "/processed/blue.pickle"
+    rouge_features_filepath_other = data_folder + "/processed/rouge.pickle"
+    r12L_features_filepath_other = data_folder + "/processed/r12L.pickle"
+    keywords_features_filepath_other = data_folder + "/processed/keywords.pickle"
+    ngrams_features_filepath_other = data_folder + "/processed/ngrams.pickle"
+    kskip_features_filepath_other = data_folder + "/processed/kskip.pickle"
 
     correct_answers = get_from_file(data_folder + 'raw/answers')
     keywords = get_from_file(data_folder + 'processed/keywords')
@@ -105,57 +161,73 @@ def main():
     for chapter in range(1, len(question_number) + 1):
         for question in range(1, question_number[chapter - 1] + 1):
             answers = get_from_file(data_folder + 'raw/' + str(chapter) + '.' + str(question))
-            grades = get_from_file(data_folder + 'scores/' + str(chapter) + '.' + str(question) + '/other')
+            grades = get_from_file(data_folder + 'scores/' + str(chapter) + '.' + str(question) + '/ave')
             answer_array.append([answers, grades])
             correct_answer_index += 1
 
-    # fileObject = open(answer_array_filepath,'wb')
+    # fileObject = open(answer_array_filepath_me,'wb')
     # for question_index in range(len(correct_answers)):
     #    for i in range(len(answer_array[question_index][1])):
     #        answer_array[question_index][0][i] = filter(answer_array[question_index][0][i], True)
     # pickle.dump(answer_array,fileObject)
     # fileObject.close()
-    fileObject = open(answer_array_filepath, 'rb')
+    fileObject = open(answer_array_filepath_other, 'rb')
     answer_array = pickle.load(fileObject)
     fileObject.close()
 
     features = pool.starmap(get_features,
                             zip(range(len(correct_answers)), repeat(answer_array), repeat(correct_answers),
                                 repeat(keywords)))
-
-    # fileObject = open(r12L_features_filepath,'wb')
-    # pickle.dump(features,fileObject)
+    # fileObject = open(rL_features_filepath_other,'rb')
+    # features = pickle.load(fileObject)
     # fileObject.close()
 
-    scores = []
-    # get an average of 10 calculations to avoid problems with randomness
-    for i in range(100):
-        for question_index in range(len(correct_answers)):
-            clf = RandomForestClassifier(n_estimators=5)
-            split_position = 13  # for it gives the best accuracy
-            clf.fit(features[question_index][0:split_position], answer_array[question_index][1][0:split_position])
-            score = clf.score(features[question_index][split_position:len(answer_array[question_index][1])],
-                              answer_array[question_index][1][split_position:len(answer_array[question_index][1])])
-            scores.append(score)
-            # print(score)
+    features_all = [features]
 
-    print("average")
-    average_score = sum(scores) / len(scores)
-    print(average_score)
+    fileObject = open(kskip_features_filepath_other, 'wb')
+    pickle.dump(features, fileObject)
+    fileObject.close()
 
-    # scores_diff_average = []
-    # for column in range(len(scores_diff_split[0])):
-    #     scores_diff_average.append(sum(row[column] for row in scores_diff_split)/len(scores_diff_split))
-
-    # print(scores_diff_average)
-    # split_positions = list(range(3, 24, 2))
-    # accuracies
-    # plt.plot(split_positions, scores_diff_average, 'bo')
-    # plt.xlabel('Split position')
-    # plt.ylabel('Average accuracy')
-    # plt.axis([2, 25, 0.3, 0.8])
-    # plt.grid(True)
-    # plt.show()
+    average_feature_scores = []
+    split_positions = list(range(3, 24, 2))
+    for features in features_all:
+        average_scores = []
+        scores_for_position = []
+        # get an average of 10 calculations to avoid problems with randomness
+        for split_position in split_positions:
+            scores = []
+            scores_for_question = []
+            for i in range(20):
+                score_for_question = []
+                for question_index in range(len(correct_answers)):
+                    # clf = RandomForestClassifier(n_estimators=5)
+                    clf = SVC()
+                    # clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                    #                    hidden_layer_sizes=(5, 2), random_state=1)
+                    features_to_train = features[question_index][0:split_position]
+                    grades_to_classify = answer_array[question_index][1][0:split_position]
+                    # for SVMs only since they need at least several classes
+                    grades_to_classify.append(596)
+                    features_to_train = np.row_stack((features_to_train, [596]))
+                    clf.fit(features_to_train, grades_to_classify)
+                    score = clf.score(features[question_index][split_position:len(answer_array[question_index][1])],
+                                      answer_array[question_index][1][
+                                      split_position:len(answer_array[question_index][1])])
+                    scores.append(score)
+                    score_for_question.append(score)
+                    # print(score)
+                average_score_for_question = sum(score_for_question) / len(score_for_question)
+                scores_for_question.append(average_score_for_question)
+            scores_for_position.append(scores_for_question)
+            average_score = sum(scores) / len(scores)
+            average_scores.append(average_score)
+        plt.boxplot(scores_for_position, positions=split_positions, sym='', widths=0.6)
+        average_feature_scores.append(average_scores)
+    plt.ylabel('Accuracy')
+    plt.xlabel('Split position')
+    plt.plot(split_positions, average_feature_scores[0], "r")
+    plt.plot(split_positions, average_feature_scores[0], "ro")
+    plt.show()
 
 
 if __name__ == "__main__":
