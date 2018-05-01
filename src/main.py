ccb6abc57b9925ab4f3ef7e42b314dc29477b847
Evgeniya Ovchinnikova <evgeniya.ovchinnikova@inf.h-brs.de>
2018-05-01 10:11:54 +0200

    #1 added problem formulation; #2 added comparison of all single features without kpca

--- src/main.py
+++ src/main.py
@@ -39,56 +39,67 @@ def get_from_file(path):
         return strings
 
 
-def ngram_cooccurrence(example, answer, ng, question_index):
+def ngram_cooccurrence(example, answer, ng):
     distance_over_ngrams = 0
-    for n in range(1, ng):
+    for n in range(1, ng + 1):
         answer_ngramed = list(ngrams(answer, n))
         example_ngramed = list(ngrams(example, n))
         summ_distance = 0
         for ngram_of_example in example_ngramed:
             dist = 0
             for ngram_of_answer in answer_ngramed:
-                dist_tmp = distance.jaccard(ngram_of_example, ngram_of_answer)
+                dist_tmp = distance.sorensen(ngram_of_example, ngram_of_answer)
                 if dist_tmp > dist:
                     dist = dist_tmp
             summ_distance += dist
-            # if (len(example_ngramed) == 0):
-            #    print("zero!")
-            #    print(example_ngramed)
-            #    print(question_index)
-    try:
-        distance_over_ngrams += summ_distance / len(example_ngramed)
-        return distance_over_ngrams
-    except:
-        return 0
+        try:
+            distance_over_ngrams += summ_distance / len(example_ngramed)
+        except:
+            distance_over_ngrams += 0
+    return distance_over_ngrams
 
 
 def kskip_ngram_cuoccurrence(answer, example, ng, kskip, question_index):
     distance_over_ngrams = 0
-    for n in range(2, ng):
+    # nltk skipgrams function doesn't include unigrams, so they are calculated separately:
+    summ_distance = 0
+    for word_ex in example:
+        dist = 0
+        for word_an in answer:
+            dist_tmp = distance.levenshtein(word_ex, word_an)
+            if dist_tmp > dist:
+                dist = dist_tmp
+        summ_distance += dist
+    try:
+        distance_over_ngrams += summ_distance / len(example)
+    except:
+        print("zero")
+        print(question_index)
+        distance_over_ngrams += 0
+    for n in range(2, ng + 1):
         answer_ngramed = list(skipgrams(answer, n, kskip))
         example_ngramed = list(skipgrams(example, n, kskip))
         summ_distance = 0
         for ngram_of_example in example_ngramed:
             dist = 0
             for ngram_of_answer in answer_ngramed:
-                dist_tmp = distance.jaccard(ngram_of_example, ngram_of_answer)
+                dist_tmp = distance.levenshtein(ngram_of_example, ngram_of_answer)
                 if dist_tmp > dist:
                     dist = dist_tmp
             summ_distance += dist
-    try:
-        distance_over_ngrams += summ_distance / len(example_ngramed)
-        return distance_over_ngrams
-    except:
-        return 0
+        try:
+            distance_over_ngrams += summ_distance / len(example_ngramed)
+        except:
+            distance_over_ngrams += 0
+    return distance_over_ngrams
 
 
-def get_keywords_count(answer, keywords, question_index):
+def get_keywords_count(answer, keywords):
     summ_distance = 0
     for keyword in keywords:
         dist = 0
         for word in answer:
-            dist_tmp = distance.sorensen(keyword, word)
+            dist_tmp = distance.levenshtein(keyword, word)
             if dist_tmp > dist:
                 dist = dist_tmp
         summ_distance += dist
@@ -119,10 +130,10 @@ def get_features(question_index, answer_array, correct_answers, keywords):
         # ROUGE_score = rouge.calc_score()
         # {'ROUGE-1': 0.45455, 'ROUGE-2': 0.2, 'ROUGE-L': 0.36364, 'ROUGE-SU4': 0.24}
         # features.append(list(ROUGE_score.values())[2])
-        ksip_ngram_cooc = kskip_ngram_cuoccurrence(correct_answers[question_index],
-                                                   answer_array[question_index][0][i], 3, 3, question_index)
+        kword_cooc = get_keywords_count(keyword,
+                                        answer_array[question_index][0][i])
         # features.append(list(ROUGE_score.values())[2])
-        features.append(ksip_ngram_cooc)
+        features.append(kword_cooc)
     # For one feature classification:
     features = np.asarray(features)
     features = features.reshape(-1, 1)
@@ -137,7 +148,6 @@ def main():
     answer_array_filepath_other = data_folder + "/processed/answer_array.pickle"
     answer_array_filepath_me = data_folder + "/processed/answer_array_me.pickle"
     answer_array_filepath_ave = data_folder + "/processed/answer_array_ave.pickle"
-    answer_array_filepath_discard = data_folder + "/processed/answer_array_discard.pickle"
     r1_features_filepath_other = data_folder + "/processed/r1.pickle"
     r2_features_filepath_other = data_folder + "/processed/r2.pickle"
     rL_features_filepath_other = data_folder + "/processed/rL.pickle"
@@ -175,18 +185,41 @@ def main():
     answer_array = pickle.load(fileObject)
     fileObject.close()
 
-    features = pool.starmap(get_features,
-                            zip(range(len(correct_answers)), repeat(answer_array), repeat(correct_answers),
-                                repeat(keywords)))
-    # fileObject = open(rL_features_filepath_other,'rb')
-    # features = pickle.load(fileObject)
-    # fileObject.close()
-
-    features_all = [features]
-
-    fileObject = open(kskip_features_filepath_other, 'wb')
-    pickle.dump(features, fileObject)
+    #features = pool.starmap(get_features, zip(range(len(correct_answers)), repeat(answer_array),
+    #                                          repeat(correct_answers), repeat(keywords)))
+    fileObject = open(r1_features_filepath_other,'rb')
+    features_r1 = pickle.load(fileObject)
     fileObject.close()
+    fileObject = open(r2_features_filepath_other, 'rb')
+    features_r2 = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(rL_features_filepath_other, 'rb')
+    features_rL = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(rS4_features_filepath_other, 'rb')
+    features_s4 = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(blue_features_filepath_other, 'rb')
+    features_blue = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(keywords_features_filepath_other, 'rb')
+    features_keywords = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(ngrams_features_filepath_other, 'rb')
+    features_ngram = pickle.load(fileObject)
+    fileObject.close()
+    fileObject = open(kskip_features_filepath_other, 'rb')
+    features_kskip = pickle.load(fileObject)
+    fileObject.close()
+
+    features_all = [features_r1, features_r2, features_rL, features_s4, features_blue, features_keywords,
+                    features_ngram, features_kskip]
+    colors = ["r", "m", "orange", "y", "b", "c", "g", "k"]
+    names = ["ROUGE-1", "ROUGE-2", "ROUGE-L", "ROUGE-SU4", "BLUE", "keywords", "bigrams", "3-skip-bigrams"]
+
+    #fileObject = open(keywords_features_filepath_other, 'wb')
+    #pickle.dump(features, fileObject)
+    #fileObject.close()
 
     average_feature_scores = []
     split_positions = list(range(3, 24, 2))
@@ -197,7 +230,7 @@ def main():
         for split_position in split_positions:
             scores = []
             scores_for_question = []
-            for i in range(20):
+            for i in range(10):
                 score_for_question = []
                 for question_index in range(len(correct_answers)):
                     # clf = RandomForestClassifier(n_estimators=5)
@@ -221,12 +254,14 @@ def main():
             scores_for_position.append(scores_for_question)
             average_score = sum(scores) / len(scores)
             average_scores.append(average_score)
-        plt.boxplot(scores_for_position, positions=split_positions, sym='', widths=0.6)
+        #plt.boxplot(scores_for_position, positions=split_positions, sym='', widths=0.6)
         average_feature_scores.append(average_scores)
     plt.ylabel('Accuracy')
     plt.xlabel('Split position')
-    plt.plot(split_positions, average_feature_scores[0], "r")
-    plt.plot(split_positions, average_feature_scores[0], "ro")
+    for feature in range(len(features_all)):
+        plt.plot(split_positions, average_feature_scores[feature], label=names[feature])
+        #plt.plot(split_positions, average_feature_scores[feature], "o")
+        plt.legend(loc='lower center')
     plt.show()
 
 
